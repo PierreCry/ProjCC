789fa7ba20fc60b0c58f258c5ca812e13a9d4457
IamHogger <jerempb@gmail.com>
2021-01-10 22:55:32 +0100

    Implémentation, ajout d'onglets + aide + responsive

--- script2.js
+++ script2.js
@@ -1,4 +1,4 @@
-function openCity(evt, project) {
+function openCity(evt,project) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
@@ -6,8 +6,61 @@ function openCity(evt, project) {
     }
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
+        tablinks[i].className = tablinks[i].className.replace("active", "");
     }
-    document.getElementById(project).style.display = "block";
+    document.getElementById("P"+project.toString()).style.display = "block";
     evt.currentTarget.className += " active";
+}
+
+var nb_fen = 1;
+
+function New() {
+
+    nb_fen++;
+
+    var tab = '<button class="t5 tablinks" onclick="openCity(event,'+ nb_fen + ')">Projet ' + nb_fen + '<close onclick="New()">&times;</close></button>';
+
+    $(".tab").append(
+        tab
+    );
+
+    $(".content").append( 
+        '<div id="P' + nb_fen + '" class="tabcontent">' +
+            '<div class="row">' +
+                '<!-- Canvas menu -->' +
+                '<div class="column3">' +
+                    '<div class="icon-bar">' +
+                        '<a href="#"><i class="fas fa-mouse-pointer"><span>Manipuler le graphe</span></i></a>' +
+                        '<a href="#"><i class="far fa-square"><span>Créer un concept</span></i></a>' +
+                        '<a href="#"><i class="fas fa-external-link-alt"><span>Créer un lien</span></i></a>' +
+                        '<a href="#"><i class="fas fa-compress-arrows-alt"><span>Recentre la carte</span></i></a>' +
+                    '</div>' +
+                '</div>' +
+                '<!-- Canvas -->' +
+                '<div class="column1">' +
+                    '<div id="canvas-container">' +
+                        '<canvas id="canvas"></canvas>' +
+                    '</div>' +
+                '</div>' +
+                '<!-- Marked for tags -->' +
+                '<div class="column2">' +
+                    '<div id="option-container">' +
+                        '<input class="t7 form-control" type="text" placeholder="Rechercher" aria-label="Rechercher" style="margin: 0px 0px 2% 0px; width: 100%;">' +
+                    '</div>' +
+                    '<div id="marked-container">' +
+                        '<textarea id="marked"></textarea>' +
+                    '</div>' +
+                '</div>' +
+            '</div>' +
+        '</div>'
+    );
+
+}
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
 }
