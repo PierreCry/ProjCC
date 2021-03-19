b72626e2bf8b61c4a1d4e4d3d5f121cc29f2b8df
PierreCry <pierrecry98@gmail.com>
2021-03-19 12:42:44 +0100

    ImportExport pour Etudiant

--- canvas.js
+++ canvas.js
@@ -326,6 +326,7 @@ class CanvasMap {
         this.kStage.add(this.kDragLayer)
 
         this.gNodes = []
+        this.gLinks = []
 
         this.kStage.scale({ x: 1.5, y: 1.5 })
         this.kStage.draw()
@@ -519,6 +520,7 @@ class CanvasMap {
     addGraphicalLink(link, redrawLayer = true) {
         const gLink = this._createGraphicalLink(link)
 
+        this.gLinks.push(gLink)
         this.kMainLayer.add(gLink.kGroup)
         if (redrawLayer) this.kMainLayer.draw()
 
@@ -712,7 +714,7 @@ class CanvasMap {
 
 function createCanvasMap(container, map = { nodes: [], links: [] }) {
     let canvasMap = new CanvasMap(container, map)
-
+    
     // Initialisation de la map
     for (const node of map.nodes) {
         canvasMap.addGraphicalNode(node, false)
@@ -737,14 +739,9 @@ function createCanvasMap(container, map = { nodes: [], links: [] }) {
             }
         }
     }
-}
 
-window.addEventListener('DOMContentLoaded', () => {
-    const mapJSON = `{"links":[{"from":0,"to":2,"verb":"entraîne"},{"from":1,"to":2,"verb":"entraîne"},{"from":2,"to":3,"verb":"entraîne"},{"from":2,"to":4,"verb":"entraîne"},{"from":4,"to":5,"verb":"entraîne"}],"nodes":[{"name":"Perte d'eau","x":138.22998992919986,"y":26.46000372314429},{"name":"Perte de Na","x":299.33999975585937,"y":26.42000000000001},{"name":"Baisse de la volémie","x":184.31998687744215,"y":131.85999493408235},{"name":"Réponse rénale","x":303.1199960327151,"y":237.87998651123132},{"name":"Baisse de la PSA","x":134.73000024414057,"y":239.1799876708992},{"name":"Tachycardie de compensation","x":95.55997955322391,"y":340.3799825439464}]}`
-    const map = JSON.parse(mapJSON)
-    const container = document.getElementById('canvas-container1')
-    createCanvasMap(container, map)
-})
+    return canvasMap;
+}
 
 function konvaHandleTextInput(kText, onInput, bold = false) {
     // Inspiré de https://konvajs.org/docs/sandbox/Editable_Text.html
--- script_etud.js
+++ script_etud.js
@@ -15,23 +15,29 @@ function openCity(evt,project) {
 var nb_fen;
 var i;
 list = new Array();
-for(i=0;i<15;i++){
-    list[i] = 0;
-}
+canvasMapList = new Array();
+
+for (i=0;i<15;i++) { list[i] = 0; }
 list[0] = 1;
 
-function New() {
+window.addEventListener('DOMContentLoaded', () => {
+    const mapJSON = `{"links":[{"from":0,"to":2,"verb":"entraîne"},{"from":1,"to":2,"verb":"entraîne"},{"from":2,"to":3,"verb":"entraîne"},{"from":2,"to":4,"verb":"entraîne"},{"from":4,"to":5,"verb":"entraîne"}],"nodes":[{"name":"Perte d'eau","x":138.22998992919986,"y":26.46000372314429},{"name":"Perte de Na","x":299.33999975585937,"y":26.42000000000001},{"name":"Baisse de la volémie","x":184.31998687744215,"y":131.85999493408235},{"name":"Réponse rénale","x":303.1199960327151,"y":237.87998651123132},{"name":"Baisse de la PSA","x":134.73000024414057,"y":239.1799876708992},{"name":"Tachycardie de compensation","x":95.55997955322391,"y":340.3799825439464}]}`
+    const map = JSON.parse(mapJSON)
+    const container = document.getElementById('canvas-container1')
+    canvasMapList.push(createCanvasMap(container, map))
+})
+
+function New(map = { nodes: [], links: [] }) {
 
     i = -1;
-    do{
+    do {
         i++;
         nb_fen = list[i];
-    }while(nb_fen!=0 && nb_fen!=10)
-    if(nb_fen==10){
-        alert("Il y a trop d'onglets pour en créer un nouveau !");
-    }
+    } while (nb_fen!=0 && nb_fen!=10)
 
-    else{
+    if (nb_fen==10) {
+        alert("Il y a trop d'onglets pour en créer un nouveau !");
+    } else {
         nb_fen = i+1;
         list[i] = nb_fen;
 
@@ -66,8 +72,9 @@ function New() {
 
         // Création d'une carte vierge dans le nouvel onglet
         const container = document.getElementById(`canvas-container${nb_fen}`);
-        createCanvasMap(container);
+        canvasMapList.push(createCanvasMap(container,map));
     }
+
 }
 
 function openNav() {
@@ -104,7 +111,7 @@ function openForm() {
         test = 1;
     }
     else {
-        document.getElementById("myForm").style.display = "none";$
+        document.getElementById("myForm").style.display = "none";
         test = 0;
     }
 }
@@ -116,6 +123,79 @@ function closeForm() {
 
 function validate() {
     document.location.href="index.html";
+}
+
+function importEtud(input) {
+    let files = input.files;
+    for (var i = 0; i < files.length; i++) {
+        let reader = new FileReader();
+        reader.readAsText(files[i]);
+        reader.onload = function() {
+            let map = JSON.parse(reader.result);
+            console.log(map);
+            New(map);
+        };
+        reader.onerror = function() {
+            console.log(reader.error);
+        };
+    }
+}
+
+function findStage(container) {
+    let gLinks = []; let gNodes = [];
+    canvasMapList.forEach(element => {
+        if (element.container = container) {
+            gLinks = element.gLinks
+            gNodes = element.gNodes
+        }
+    });
+    return [gLinks,gNodes];
+}
+
+function writeJSON(gAll) {
+
+    // link
+    let JSON = `{"links":[`
+    gAll[0].forEach(element => {
+        link = element.link;
+        JSON = JSON + `{"from":` + link.from + `,"to":` + link.to + `,"verb":"` + link.verb + `"},`;
+    });
+    JSON = JSON.slice(0,-1);
+
+    // node
+    JSON = JSON + `],"nodes":[`
+    gAll[1].forEach(element => {
+        node = element.node;
+        JSON = JSON + `{"name":"` + node.name + `","x":` + node.x + `,"y":` + node.y + `},`;
+    });
+    JSON = JSON.slice(0,-1);
+    JSON = JSON + `]}`
+
+    return JSON
+
+}
+
+function canvas_to_json() {
+    const container = document.getElementById(`canvas-container1`);
+    let gAll = findStage(container);
+    if (gAll[0].length == 0 && gAll[1].length == 0) {
+        window.alert("Rien à exporter")
+    } else {
+        return writeJSON(gAll);
+    }
+}
+
+function download(content, fileName, contentType) {
+    const a = document.createElement("a");
+    const file = new Blob([content],{type:contentType});
+    a.href = URL.createObjectURL(file);
+    a.download = fileName;
+    a.click();
+}
+
+function exportEtud() {
+    var json = canvas_to_json();
+    download(json, "json-file-name.json", "text/plain");
 }
 
 $(document).ready(function() {
