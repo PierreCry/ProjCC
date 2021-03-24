e80257461b82a0c9528eb7836cc5f60d395abf77
nurkinali <nurkinali81@gmail.com>
2021-03-24 12:24:20 +0100

    Note Added

--- Interface_etudiant.php
+++ Interface_etudiant.php
@@ -27,8 +27,8 @@
  $ExamID = "SecondeSession";
  $JSON = '{"links":[{"from":0,"to":2,"verb":"entraîne"},{"from":1,"to":2,"verb":"entraîne"},{"from":2,"to":3,"verb":"entraîne"},{"from":2,    "to":4,"verb":"entraîne"},{"from":4,"to":5,"verb":"entraîne"}],"nodes":[{"name":"Perte d\'eau","x":138.22998992919986,"y":26.                46000372314429},{"name":"Perte de Na","x":299.33999975585937,"y":26.42000000000001},{"name":"Baisse de la volémie","x":184.31998687744215,   "y":131.85999493408235},{"name":"Réponse rénale","x":303.1199960327151,"y":237.87998651123132},{"name":"Baisse de la PSA","x":134.           73000024414057,"y":239.1799876708992},{"name":"Tachycardie de compensation","x":95.55997955322391,"y":340.3799825439464}]}';
 
-$conn = mysqli_connect('localhost:3306', 'pi', 'lmao', '');
-// $conn = mysqli_connect('localhost:3307', 'root', '', '');
+// $conn = mysqli_connect('localhost:3306', 'pi', 'lmao', '');
+$conn = mysqli_connect('localhost:3307', 'root', '', '');
 
 $JSON = mysqli_real_escape_string($conn, $JSON);
 
@@ -36,14 +36,14 @@ $JSON = mysqli_real_escape_string($conn, $JSON);
     die("Connection failed: <br>". mysqli_connect_error());
   }
  
-  $sql = "CREATE DATABASE IF NOT EXISTS ConceptMaps";
+  $sql = "CREATE DATABASE IF NOT EXISTS ConceptMaps1";
   if ($conn->query($sql) === TRUE) {
   //  echo "Database created successfully <br>";
   } else {
   //  echo "Error creating database: <br>" . $conn->error;
   }
  
-  $conn -> select_db("ConceptMaps");
+  $conn -> select_db("ConceptMaps1");
  
   // Pass StudentID from script_login.js
   $StudentID = $_POST['data'];
@@ -69,11 +69,11 @@ $JSON = mysqli_real_escape_string($conn, $JSON);
   //  echo "Error creating table: <br>" . $conn->error;
   }
  
-  $sql = "CREATE TABLE IF NOT EXISTS `TableJointure` (`StudentID` VARCHAR(255) NOT NULL, `ClassID` VARCHAR(255) NOT NULL, `ExamID`             VARCHAR(255) NOT NULL, `JSON` TEXT(4294967295), CONSTRAINT `uniqueAttributes` UNIQUE (StudentID, `ClassID`, `ExamID`, JSON(255))) CHARSET =  utf8";
+  $sql = "CREATE TABLE IF NOT EXISTS TableJointure (`StudentID` VARCHAR(255) NOT NULL, `ClassID` VARCHAR(255) NOT NULL, `ExamID` VARCHAR(255) NOT NULL, `JSON` TEXT(4294967295), `Note` VARCHAR(255), CONSTRAINT `uniqueAttributes` UNIQUE (StudentID, `ClassID`, `ExamID`, JSON(255))) CHARSET =  utf8";
   if ($conn->query($sql) === TRUE) {
   //  echo "Table created successfully <br>";
   } else {
-    echo "Error creating table: <br>" . $conn->error;
+  // echo "Error creating table: <br>" . $conn->error;
   }
  
   $sql = "INSERT INTO Student (StudentID) VALUES ('$StudentID')";
@@ -97,7 +97,7 @@ $JSON = mysqli_real_escape_string($conn, $JSON);
   //  echo "Error inserting informations: <br>" . $conn->error;
   }
  
-  $sql = "INSERT INTO TableJointure (StudentID, ClassID, ExamID, JSON) VALUES ('$StudentID', '$ClassID', '$ExamID', '$JSON')";
+  $sql = "INSERT INTO TableJointure (StudentID, ClassID, ExamID, JSON, Note) VALUES ('$StudentID', '$ClassID', '$ExamID', '$JSON', '')";
   if ($conn->query($sql) === TRUE) {
   //  echo "Informations inserted successfully <br>";
   } else {
