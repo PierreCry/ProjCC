c160479c1504a0287117d61b378319624cebd9fa
PierreCry <pierrecry98@gmail.com>
2021-03-24 23:38:06 +0100

    export csv

--- script_corr.js
+++ script_corr.js
@@ -1,3 +1,39 @@
+
+class Concepts {
+    constructor(label,value) {
+        this.label = label;
+        this.value = value;
+        this.synonyms = [];
+    }
+    get label() { return this.label; }
+    get value() { return this.value; }
+    get synonyms() { return this.synonyms; }
+    addSynonyms(label) { this.synonyms.push(new Concepts(label,this.value)); }
+}
+
+class Liens {
+    constructor(verb,preLabel,postLabel,value) {
+        this.verb = verb;
+        this.preLabel = preLabel;
+        this.postLabel = postLabel;
+        this.value = value;
+        this.synonyms = [];
+    }
+    get verb() { return this.verb; }
+    get preLabel() { return this.preLabel; }
+    get postLabel() { return this.postLabel; }
+    get value() { return this.value; }
+    get synonyms() { return this.synonyms; }
+    addSynonyms(verb,preLabel,postLabel) { this.synonyms.push(new Liens(verb,preLabel,postLabel,this.value)); }
+}
+
+var ConceptsValide = [new Concepts("CV1",1),new Concepts("CV2",1)];
+var ConceptsNeutre = [new Concepts("CN1",0),new Concepts("CN2",0)];
+var ConceptsInvalide = [new Concepts("CI1",2),new Concepts("CI2",2)];
+var LiensValide = [new Liens("LV1verb","LV1pre","LV1post",1),new Liens("LV2verb","LV2pre","LV2post",1)];
+var LiensNeutre = [new Liens("LN1verb","LN1pre","LN1post",0),new Liens("LN2verb","LN2pre","LN2post",0)];
+var LiensInvalide = [new Liens("LI1verb","LI1pre","LI1post",2),new Liens("LI2verb","LI2pre","LI2post",2)];
+ 
 function openTab(evt,project) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
@@ -286,12 +322,26 @@ for (i = 0; i < close.length; i++) {
   }
 }
 
-/*
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
-*/
+function exportCorr() {
+
+    var csv = ["filename","\n"];
+    for (var i=0; i<ConceptsValide.length; i++) {csv.push(ConceptsValide[i].label,ConceptsValide[i].value)};
+    for (var i=0; i<ConceptsNeutre.length; i++) {csv.push(ConceptsNeutre[i].label,ConceptsNeutre[i].value)};
+    for (var i=0; i<ConceptsInvalide.length; i++) {csv.push(ConceptsInvalide[i].label,ConceptsInvalide[i].value)};
+    for (var i=0; i<LiensValide.length; i++) {csv.push(LiensValide[i].verb,LiensValide[i].preLabel,LiensValide[i].postLabel,LiensValide[i].value)};
+    for (var i=0; i<LiensNeutre.length; i++) {csv.push(LiensNeutre[i].verb,LiensNeutre[i].preLabel,LiensNeutre[i].postLabel,LiensNeutre[i].value)};
+    for (var i=0; i<LiensInvalide.length; i++) {csv.push(LiensInvalide[i].verb,LiensInvalide[i].preLabel,LiensInvalide[i].postLabel,LiensInvalide[i].value)};
+
+    var csvFile;
+    var downloadLink;
+
+    csvFile = new Blob([csv], {type: "text/csv"});
+    downloadLink = document.createElement("a");
+    downloadLink.download = "filename.csv";
+    downloadLink.href = window.URL.createObjectURL(csvFile);
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+
+}
+
